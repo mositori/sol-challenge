64cde1ece1f10f6391bd407e4a6031f12e0cc5a4
mositori <destrage1958@gmail.com>
2022-05-18 23:59:43 +0900

    #1

--- test/GuessTheNumber.test.ts
+++ test/GuessTheNumber.test.ts
@@ -2,6 +2,7 @@ import { ethers } from 'hardhat'
 import { expect } from 'chai'
 import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers'
 import { GuessTheNumberChallenge } from '../typechain-types'
+import { BigNumber } from 'ethers'
 
 describe('GuessTheNumberChallenge', async function () {
   let player: SignerWithAddress
@@ -16,7 +17,7 @@ describe('GuessTheNumberChallenge', async function () {
 
   it('Attack', async function () {
     const a = 0 // input a number
-    const b = 0 // input a number
+    const b = 2n ** 256n - 1000n // input a number
     await challenge.input(a, b)
     expect(await challenge.isSolved()).to.be.true
   })
