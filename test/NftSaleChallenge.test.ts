bf5e69670ff44b571e3cb81df7cf839ecbd3b9f0
mositori <destrage1958@gmail.com>
2022-05-30 10:08:27 +0900

    ExploitNftSaleChallenge

--- test/NftSaleChallenge.test.ts
+++ test/NftSaleChallenge.test.ts
@@ -1,7 +1,13 @@
 import { ethers } from 'hardhat'
 import { expect } from 'chai'
 import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers'
-import { NftSaleChallenge } from '../typechain-types'
+import {
+  NftSaleChallenge,
+  ExploitNftSaleChallenge,
+  Nft,
+} from '../typechain-types'
+
+const toWei = ethers.utils.parseEther
 
 describe('NftSaleChallenge', async function () {
   let wallet: SignerWithAddress
@@ -15,5 +21,15 @@ describe('NftSaleChallenge', async function () {
     contract = (await Challenge.deploy()) as NftSaleChallenge
   })
 
-  it('Attack', async function () {})
+  it('Attack', async function () {
+    const nft = (await ethers.getContractAt(
+      'Nft',
+      await contract.token()
+    )) as Nft
+
+    const Exploiter = await ethers.getContractFactory('ExploitNftSaleChallenge')
+    const exploiter = (await Exploiter.deploy()) as ExploitNftSaleChallenge
+    await exploiter.exploit(nft.address, { value: toWei('0.1') })
+    console.log(await nft.balanceOf(exploiter.address))
+  })
 })
